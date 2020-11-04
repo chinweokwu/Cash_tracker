f9317bad3ccadd7b1d71a0ef45795bd243def38e
chinweokwu <paulcmorah@gmail.com>
2020-11-04 22:25:38 +0100

    added issue created by Temitayo Ojo

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,7 @@ class HomesController < ApplicationController
   def external
     user = User.find_by(id: current_user.id)
     @external_transactions = user.transactions.where(group_id: nil).order(created_at: :desc)
+    @total_nongroup = @external_transactions.includes([:group]).sort_by(&:created_at).reverse
     @total_ex_trans = @external_transactions.sum(:amount)
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,21 @@ module ApplicationHelper
   def user_total_trans(user, amount)
     user.transactions.sum(amount)
   end
+
+  def all_trans(transaction, value)
+    if transaction.group_id.value == nil
+          transaction[group_id] = "none"
+    else
+          transaction[group_name]
+    end
+  end
+
+  def all_trans=(value)
+    if transaction.value.empty?
+      write_attribute(:name, nil)
+    else
+      super
+    end
+  end
 end
 # rubocop:enable Style/GuardClause
